c7e56dee7200a154814cc8d7bef2e454a0b4291b
Michael Steenbeek <m.o.steenbeek@gmail.com>
2020-08-11 01:41:30 +0200

    Start work on making the plug-in network safe

--- everything-must-die.js
+++ everything-must-die.js
@@ -17,27 +17,56 @@ var destroyAllFootpathItems = function() {
     }
 }
 
-// Exact replication of the EXPLODE!!! cheat.
-var explode = function() 
+var explodeGA = function(isExecuting, args)
 {
-    for (var i = 0; i < map.numEntities; i++) 
+    if (isExecuting)
     {
-        var entity = map.getEntity(i);
-        if (!entity)
+        for (var i = 0; i < map.numEntities; i++)
         {
-            continue;
+            var entity = map.getEntity(i);
+            if (!entity)
+            {
+                continue;
+            }
+
+            var entityIsGuest = entity.type === 'peep' && entity.peepType === "guest";
+
+            if (entityIsGuest && context.getRandom(0, 6) === 0)
+            {
+                entity.setFlag("explode", true);
+            }
         }
-        
-        var entityIsGuest = entity.type === 'peep' && entity.peepType === "guest";
-        
-        if (entityIsGuest && context.getRandom(0, 6) === 0) 
-        {
-            entity.setFlag("explode", true);
+    }
+    
+    return {
+        cost: 0,
+        expenditureType: "landscaping",
+        position: {
+            x: -1,
+            y: -1,
+            z: 0 
         }
     }
 }
 
-var main = function() {
+// Exact replication of the EXPLODE!!! cheat.
+var explode = function() 
+{
+    context.executeAction("explode-guests", {}, function() {});
+}
+
+var main = function() 
+{
+    context.registerAction(
+        "explode-guests",
+        function(args) {
+            return explodeGA(false, args);
+        },
+        function(args) {
+            return explodeGA(true, args);
+        }
+    );
+    
     ui.registerMenuItem("Destroy path furniture", destroyAllFootpathItems);
     ui.registerMenuItem("Explode guests", explode);
 };
